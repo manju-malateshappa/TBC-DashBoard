2585ad830d0b75774d1c1cfd33a862f65a129a01
manjum2050 <manjum2050@gmail.com>
2019-03-05 10:41:46 +0530

    This CL covers upto mid of Binding in ES6

---
+++ src/components/activities/ActivitiesPage.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+
+class ActivitiesPage extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1> Activities </h1>
+            </div>
+        );
+    }
+}
+
+export default ActivitiesPage;
---
+++ src/components/common/Header.js
@@ -0,0 +1,18 @@
+import React, { PropTypes } from 'react';
+import { Link, IndexLink } from 'react-router';
+
+const Header = () => {
+    return (
+        <nav>
+            <IndexLink to="/" activeClassName="active">Home</IndexLink>
+            {" | "}
+            <Link to="/activities" activeClassName="active">Activities</Link>
+            {" | "}
+            <Link to="/TellYourStory" activeClassName="active">Your Story</Link>
+            {" | "}
+            <Link to="/about" activeClassName="active">About</Link>
+        </nav>
+    );
+};
+
+export default Header;
